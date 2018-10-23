ae179daa903432cbe0e99a50b099ed4d97c58016
Chris Gray <chrisjgray@gmail.com>
2018-10-23 11:41:48 -0500

    Fix Test

--- HelloWorldTests.py
+++ HelloWorldTests.py
@@ -1,8 +1,8 @@
 import unittest
-from HelloWorld import Hello
+from HelloWorld import HelloWorld
 class TestCase(unittest.TestCase):
     def test_default_hello(self):
-        hello = Hello()
+        hello = HelloWorld()
         self.assertEqual(hello.message, 'Hello World')
 
 if __name__ == '__main__':
